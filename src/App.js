9243bf294ae9e43807cd4b1b7b8d578775655730
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2022-11-08 21:38:23 +0300

    added history component to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.scss';
 import Header from "./Components/Header/Header";
 import Cards from "./Components/Cards/Cards";
+import History from "./Components/History/History";
 
 const App = () => {
     return (
         <div className="App">
             <Header/>
             <Cards/>
+            <History/>
         </div>
     );
 }
