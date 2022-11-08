4066743c49c7affeca773daa1142a6b2e5108b3b
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2022-11-08 21:37:30 +0300

    Created History.jsx and module.scss

---
+++ src/Components/History/History.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import classes from './History.module.css';
+
+
+
+const History = () => {
+    return (
+        <div className={classes.history}>
+
+        </div>
+    )
+}
+
+
+
+export default History;
