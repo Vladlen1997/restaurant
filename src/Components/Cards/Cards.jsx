a1e822d2ca45fdae57467a40700d4507e6fe1d1d
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2022-11-08 19:57:58 +0300

    created a card component

---
+++ src/Components/Cards/Cards.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import classes from './Cards.module.scss';
+
+
+const Cards = () => {
+    return (
+        <div className={classes.cards}>
+
+        </div>
+    )
+}
+
+
+
+export default Cards;
