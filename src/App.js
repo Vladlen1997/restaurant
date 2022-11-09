677c6533f8aeb8ed68f8084fd3054249d71458cb
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2022-11-09 10:52:12 +0300

    Added Dishes, BlackBlock and Menu Components

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import './App.scss';
 import Header from "./Components/Header/Header";
 import Cards from "./Components/Cards/Cards";
 import History from "./Components/History/History";
+import BlackBlock from "./Components/BlackBlock/BlackBlock";
+import Dishes from "./Components/Dishes/Dishes";
+import Menu from "./Components/Menu/Menu";
 
 const App = () => {
     return (
         <div className="App">
-            <Header/>
-            <Cards/>
-            <History/>
+
+            <div className="app-container">
+                <Header/>
+                <Cards/>
+                <History/>
+                <BlackBlock/>
+                <Dishes />
+                <Menu/>
+            </div>
         </div>
     );
 }
