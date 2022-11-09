a0f6c4888b089198246e8a2e20355acbe7039b14
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2022-11-09 10:52:44 +0300

    Created Menu.jsx and Menu.module.scss

---
+++ src/Components/Menu/Menu.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import classes from './Menu.module.scss';
+
+
+const Menu = () => {
+    return (
+        <div className={classes.menu}>
+
+        </div>
+    )
+}
+
+
+
+export default Menu;
