113e311eb7fca7d0f62e1dd61d7f8e86782c7fc2
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2022-11-09 14:19:48 +0300

    Added layout to the menu component

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -5,11 +5,89 @@ import classes from './Menu.module.scss';
 const Menu = () => {
     return (
         <div className={classes.menu}>
+            <div className={classes.container}>
+                <div className={classes.menu_title}>
+                    Наше меню
+                </div>
 
+                <div className={classes.menu_items}>
+
+                    <div className={classes.menu_item}>
+                        <div className={classes.menu_image}>
+                            <img src="/img/menu_item.png" alt="menu-img"/>
+                            <div className={classes.menu_price}>
+                                <img src="/img/price.png" alt="price"/>
+                                <div className={classes.price}>420</div>
+                            </div>
+
+                        </div>
+
+                        <div className={classes.menu_text}>Гамбургер макси</div>
+
+                        <div className={classes.menu_subtext}>
+                            Максимально толстый <br/> слой мяса
+                        </div>
+
+
+                        <div className={classes.menu_button}>
+                            <a href="#">Заказать</a>
+                        </div>
+                    </div>
+
+
+
+                    <div className={classes.menu_item}>
+                        <div className={classes.menu_image}>
+                            <img src="/img/menu_item.png" alt="menu-img"/>
+                            <div className={classes.menu_price}>
+                                <img src="/img/price.png" alt="price"/>
+                                <div className={classes.price}>420</div>
+                            </div>
+
+                        </div>
+
+                        <div className={classes.menu_text}>Гамбургер макси</div>
+
+                        <div className={classes.menu_subtext}>
+                            Максимально толстый <br/> слой мяса
+                        </div>
+
+
+                        <div className={classes.menu_button}>
+                            <a href="#">Заказать</a>
+                        </div>
+                    </div>
+
+
+
+
+                    <div className={classes.menu_item}>
+                        <div className={classes.menu_image}>
+                            <img src="/img/menu_item.png" alt="menu-img"/>
+                            <div className={classes.menu_price}>
+                                <img src="/img/price.png" alt="price"/>
+                                <div className={classes.price}>420</div>
+                            </div>
+
+                        </div>
+
+                        <div className={classes.menu_text}>Гамбургер макси</div>
+
+                        <div className={classes.menu_subtext}>
+                            Максимально толстый <br/> слой мяса
+                        </div>
+
+
+                        <div className={classes.menu_button}>
+                            <a href="#">Заказать</a>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
         </div>
     )
 }
-
 
 
 export default Menu;
