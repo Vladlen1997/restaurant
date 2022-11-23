5827280a06ac4bf294e0834c8b14e0cd918c0d21
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2022-11-23 12:20:52 +0300

    added an event for closing the cart on onClick

--- src/Components/CartPage/CartPage.jsx
+++ src/Components/CartPage/CartPage.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import classes from './CartPage.module.scss';
 
 
-const CartPage = () => {
+const CartPage = (props) => {
     return (
 
             <div className={classes.overlay}>
                 <div className={classes.cart}>
-
+                        <div className={classes.header}>
+                            <h2>Корзина</h2>
+                            <img onClick={props.onClose} src="/img/cancel.svg" alt="cancel"/>
+                        </div>
                 </div>
             </div>
 
