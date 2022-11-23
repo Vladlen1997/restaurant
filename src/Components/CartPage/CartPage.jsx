b88e78479f8a7ba266862223fa035a9707ca7c35
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2022-11-23 11:53:04 +0300

    Created cart page and styles

---
+++ src/Components/CartPage/CartPage.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import classes from './CartPage.module.scss';
+
+
+const CartPage = () => {
+    return (
+
+            <div className={classes.overlay}>
+                <div className={classes.cart}>
+
+                </div>
+            </div>
+
+    )
+}
+
+
+
+export default CartPage
